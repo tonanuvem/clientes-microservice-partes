fad78ee5132c1f3c71fa1a233d521cb5fd509040
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-02-03 16:20:40 -0300

    Update run5.sh

--- run5.sh
+++ run5.sh
@@ -1,7 +1,13 @@
 docker run -d -p 27017:27017 --name mongodb mongo
 #source venv_clientes/bin/activate
-SERVER_IP=$(curl checkip.amazonaws.com)
-echo ""
-echo "    Acessar: http://$SERVER_IP:5000/ "
-echo ""
-python3 clientes_microservice-parte5_BD/server.py
+
+cd 
+npm start &
+python3 ../clientes_microservice-parte5_BD/server.py &
+wait
+
+#SERVER_IP=$(curl checkip.amazonaws.com)
+#echo ""
+#echo "    Acessar: http://$SERVER_IP:5000/ "
+#echo ""
+#python3 clientes_microservice-parte5_BD/server.py
