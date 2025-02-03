43dac18a86923827f7e82a7ab2a4b2e5f4c13c95
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-02-03 15:18:21 -0300

    Update run3.sh

--- run3.sh
+++ run3.sh
@@ -1,6 +1,17 @@
 #source venv_clientes/bin/activate
-SERVER_IP=$(curl checkip.amazonaws.com)
-echo ""
-echo "    Acessar: http://$SERVER_IP:5000/ "
-echo ""
-python3 clientes_microservice-parte3_SPA/server.py
+
+npx -y create-react-app clientes_admin && cd clientes_admin
+npm i react-admin ra-data-simple-rest @mui/material @emotion/react @emotion/styled
+
+rm clientes_admin/src/App.js
+cp clientes_microservice-parte3_SPA/src/App.js clientes_admin/src/App.js
+
+npm start &
+python3 clientes_microservice-parte3_SPA/server.py &
+
+#SERVER_IP=$(curl checkip.amazonaws.com)
+#echo ""
+#echo "    Acessar: http://$SERVER_IP:5000/ "
+#echo ""
+
+wait
